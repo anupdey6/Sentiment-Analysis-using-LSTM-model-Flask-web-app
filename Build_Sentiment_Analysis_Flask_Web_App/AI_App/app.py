6f8dd1a613918f505a5f21dd986db5cab596e316
anupdey6 <anup.dey6@gmail.com>
2021-03-28 21:14:00 +0200

    save

--- Build_Sentiment_Analysis_Flask_Web_App/AI_App/app.py
+++ Build_Sentiment_Analysis_Flask_Web_App/AI_App/app.py
@@ -9,8 +9,6 @@ from keras.datasets import imdb
 from keras.preprocessing import sequence
 from keras.models import load_model
 
-
-##
 IMAGE_FOLDER = os.path.join('static', 'img_pool')
 
 app = Flask(__name__)
