3d903bac8408c67970ee3f4056c327fb3eaa7021
anupdey6 <anup.dey6@gmail.com>
2021-03-29 10:17:28 +0200

    save

--- AI_App/app.py
+++ AI_App/app.py
@@ -9,6 +9,8 @@ from keras.datasets import imdb
 from keras.preprocessing import sequence
 from keras.models import load_model
 
+
+##
 IMAGE_FOLDER = os.path.join('static', 'img_pool')
 
 app = Flask(__name__)
